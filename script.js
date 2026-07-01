436b348a4ff640d3188df50ccf8526735a2713c6
lalo <redleenicko123@gmail.com>
2026-07-01 21:43:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy see you on sunday dams!! :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
